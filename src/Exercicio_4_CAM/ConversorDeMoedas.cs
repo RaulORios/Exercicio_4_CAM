08564b6a1e41704d7a2665b24c9f8182878e3877
RaulORios <raul.orios89@outlook.com>
2021-09-20 15:12:52 -0300

    Arquivos atualizados

---
+++ src/Exercicio_4_CAM/ConversorDeMoedas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_4_CAM
+{
+    class ConversorDeMoedas
+    {
+        public double IOF = 6.0;
+        public double Cotacao;
+        public double Dollars;
+
+        public double Conversor()
+        {
+            return Dollars * Cotacao;
+        }
+
+        public double AjusteIOF()
+        {
+            return Conversor() * 0.06;
+        }
+
+        public double ValorFinal()
+        {
+            return Conversor() + AjusteIOF();
+        }
+    }
+}
